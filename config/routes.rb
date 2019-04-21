cdd2550c19e0eb118980a6f410764241de034413
ch2isk4kos <ch2isk4kos@gmail.com>
2019-04-21 17:57:24 -0400

    generated posts_controller.rb && created _post partial and rendered to pages #index page

--- config/routes.rb
+++ config/routes.rb
@@ -1,17 +1,28 @@
 Rails.application.routes.draw do
 
+    # localhost:3000
+    root 'pages#index'
+
+    # DEVISE :user
     devise_for :users, :controllers => {:registrations => "registrations"}
 
     # localhost:3000/signup
     devise_scope :user do
         get 'signup', to: 'devise/registrations#new'
     end
-    
+
     # localhost:3000/login
     devise_scope :user do
         get 'login', to: 'devise/sessions#new'
     end
 
-    # localhost:3000
-    root 'pages#index'
+    # POSTS
+    resources :posts do
+        collection do
+            get 'hobby'
+            get 'study'
+            get 'team'
+        end
+    end
+    
 end
