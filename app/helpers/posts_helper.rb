53a909c568627479d958c05ca66a99f35da7e13d
ch2isk4kos <ch2isk4kos@gmail.com>
2019-04-23 19:51:39 -0400

    added all branches to the home page

--- app/helpers/posts_helper.rb
+++ app/helpers/posts_helper.rb
@@ -1,7 +1,7 @@
 module PostsHelper
 
-    def no_posts_partial_path
-        @posts.empty? ? 'posts/branch/no_posts' : 'shared/empty_partial'
+    def no_posts_partial_path(posts)
+        posts.empty? ? 'posts/shared/no_posts' : 'shared/empty_partial'
     end
 
     def post_format_partial_path
