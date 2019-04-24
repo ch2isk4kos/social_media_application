ab658f726eba4d5f9f52a3b330d4eff4362a9a28
ch2isk4kos <ch2isk4kos@gmail.com>
2019-04-23 20:54:14 -0400

    created && migrated Private::Conversation and Private::Message models for instant messaging ++ defined Associations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,9 @@
 class User < ApplicationRecord
     has_many :posts, dependent: :destroy
 
+    has_many :private_messages, class_name: 'Private::Message'
+    has_many  :private_conversations, foreign_key: :sender_id, class_name: 'Private::Conversation'
+
     # Include default devise modules. Others available are:
     # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
     devise :database_authenticatable, :registerable,
